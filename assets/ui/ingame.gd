5a572908e05f916e5e25c58b32b4f90a49758e44
Sander in 't Veld <sanderintveld@hotmail.com>
2022-09-11 16:11:36 +0200

    Blocked out in-game UI with pixel art panels, portraits and personnel files.

---
+++ assets/ui/ingame.gd
@@ -0,0 +1,16 @@
+extends Control
+
+
+var settings = Settings.new()
+
+
+func _ready():
+	settings.load_all()
+	if settings.get_value("graphics", "enable_fullscreen", false):
+		OS.window_fullscreen = true
+	if settings.get_value("accessibility", "screen_reader_enabled", true):
+		$ScreenReader.enabled = true
+		#$ScreenReader.TTS.speak("Gameplay", true)
+		$ScreenReader.set_initial_screen_focus()
+	elif InputController.prefer_joypad_over_keyboard:
+		$ScreenReader.set_initial_screen_focus()
