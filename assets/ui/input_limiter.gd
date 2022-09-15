22b60754e31e336cc56a338c783f3962cf64137b
Sander in 't Veld <sanderintveld@hotmail.com>
2022-09-15 17:23:09 +0200

    Rewrote InputLimiter to manually gobble up inputs instead of using a Timer and set_disable_input. Show INPUT BLOCKED when double clicking.

--- assets/ui/input_limiter.gd
+++ assets/ui/input_limiter.gd
@@ -1,39 +1,91 @@
 class_name InputLimiter
-extends Timer
+extends Node
 
 var is_cooldown_enabled = false
-var cooldown_period = 0.5
+var cooldown_duration = 0.5
 
 var _is_functional = true
+var _cooldown_end_delay = 0
+var _was_disabled_this_frame = false
+var _info_highlight_end_delay = null
+
+const INFO_HIGHLIGHT_DURATION = 0.1
 
 
 func _ready():
-	var err = self.connect("timeout", self, "_on_timeout")
-	if err != OK:
-		printerr("failed to connect timeout: ", err)
-		self._is_functional = false
+	$Info.visible = false
 
 
 func _input(event):
 	if not self.is_cooldown_enabled or not self._is_functional:
 		return
-	# Disable input after key release (so that the pressed event triggers).
-	if event is InputEventKey and not event.pressed:
+	if _cooldown_end_delay > 0 and not _was_disabled_this_frame:
+		if event is InputEventKey:
+			_info_highlight_end_delay = INFO_HIGHLIGHT_DURATION
+			get_tree().set_input_as_handled()
+		if event is InputEventJoypadButton:
+			_info_highlight_end_delay = INFO_HIGHLIGHT_DURATION
+			get_tree().set_input_as_handled()
+		if event is InputEventMouseButton:
+			_info_highlight_end_delay = INFO_HIGHLIGHT_DURATION
+			$Info.rect_position = (get_viewport().get_mouse_position() +
+				Vector2(20, 0))
+			get_tree().set_input_as_handled()
+		return
+	if event.is_action_pressed("ui_focus_next"):
 		_disable_input()
-	if event is InputEventJoypadButton and not event.pressed:
+	if event.is_action_pressed("ui_focus_prev"):
+		_disable_input()
+	if event.is_action_pressed("ui_left"):
+		_disable_input()
+	if event.is_action_pressed("ui_right"):
+		_disable_input()
+	if event.is_action_pressed("ui_up"):
+		_disable_input()
+	if event.is_action_pressed("ui_down"):
+		_disable_input()
+	if event.is_action_pressed("focus_rewind_button"):
+		_disable_input()
+	if event.is_action_pressed("focus_personnel_files"):
+		_disable_input()
+	if event.is_action_pressed("focus_answer1"):
+		_disable_input()
+	if event.is_action_pressed("focus_answer2"):
+		_disable_input()
+	if event.is_action_pressed("focus_answer3"):
 		_disable_input()
 
 
-func enable_input():
-	get_tree().get_root().set_disable_input(false)
+func _process(delta):
+	if _cooldown_end_delay > 0:
+		if _info_highlight_end_delay != null:
+			$Info.visible = true
+			$Info/Bar.rect_size.x = $Info/Label.rect_size.x * min(1,
+				_cooldown_end_delay / (0.8 * self.cooldown_duration))
+			if _info_highlight_end_delay > 0:
+				var highlight = (_info_highlight_end_delay
+					/ INFO_HIGHLIGHT_DURATION)
+				$Info.modulate.a = 0.5 + 0.5 * highlight
+				_info_highlight_end_delay -= delta
+			else:
+				$Info.modulate.a = 0.5
+		else:
+			$Info.visible = false
+		_cooldown_end_delay -= delta
+	else:
+		$Info.visible = false
+		_info_highlight_end_delay = null
+		$Info.rect_position = Vector2(10, 10)
+	_was_disabled_this_frame = false
+
 
 func _disable_input():
 	if not self.is_cooldown_enabled or not self._is_functional:
 		return
-	if self.time_left > 0:
+	if _cooldown_end_delay > 0:
 		return
-	get_tree().get_root().set_disable_input(true)
-	self.start(self.cooldown_period)
+	_cooldown_end_delay = self.cooldown_duration
+	_was_disabled_this_frame = true
 
 
 func configure(settings):
@@ -42,14 +94,11 @@ func configure(settings):
 	if self.is_cooldown_enabled:
 		_disable_input()
 	else:
-		enable_input()
+		_cooldown_end_delay = -1
+
 
 func trigger():
 	_disable_input()
-
-
-func _on_timeout():
-	enable_input()
 
 
 func _on_any_button_pressed():
